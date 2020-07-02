de9dd45d6e083a806151e665cf842476a6f6e524
Rohan Sawant <rohansawantct83@gmail.com>
2020-07-02 12:07:21 +0530

    Tests passing

--- truffle-config.js
+++ truffle-config.js
@@ -48,6 +48,12 @@ module.exports = {
      network_id: "*",       // Any network (default: none)
     },
 
+    // development: {
+    //  host: "127.0.0.1",
+    //  port: 8545,
+    //  network_id: "1",
+    // },
+
     // Another network with more advanced options...
     // advanced: {
       // port: 8777,             // Custom port
